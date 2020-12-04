ed758666e1df70abaac5e0e4686515b0e05d0398
yadavthyag <yadavthyagaraj@gmail.com>
2020-12-04 19:27:27 +0530

    Create printStringPyramid.js

---
+++ recipes/printStringPyramid.js
@@ -0,0 +1,32 @@
+console.log("String Pyramid");
+
+
+let li = "";
+
+function printXes(sizeOfx, charValue) {
+	// Creating a array
+	console.log(li); // [1, 2] 
+
+	for (let i = 0; i < sizeOfx; i++) {
+		// create the elements first
+		li = li + " ";
+		//console.log(l1); 
+
+	}
+
+
+	for (let i = sizeOfx; i >= 0; i--) {
+		try {
+			// Replace elements between with the last charter
+			li = li.substring(0, i) + charValue + li.substring(i, li.length);
+			console.log(li);
+		} catch (e) { }
+
+
+	}
+
+}
+
+
+printXes(20, "p");
+
