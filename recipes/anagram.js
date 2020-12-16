a5c4e72e58f6bfd3d2fd0299c0eea6de2f88383e
yadavthyagaraj <74851892+yadavthyag@users.noreply.github.com>
2020-12-16 16:59:50 +0530

    Update anagram.js

--- recipes/anagram.js
+++ recipes/anagram.js
@@ -1,20 +1,28 @@
- 
-console.log("Are the given two words an Anagram");
+console.log("Are two passed stirngs an anagram or not");
 
-function isAnagramDeclarative(valueInp1,valueInp2){
+ function isAnagram(valueInp1,valueInp2){
    valueInp1 = valueInp1.toLowerCase();
    valueInp2 = valueInp2.toLowerCase();
 
+   console.log("sortStrungCust(valueInp1) : "+sortStrungCust(valueInp1));
+   console.log("sortStrungCust(valueInp2) : "+sortStrungCust(valueInp2));
 
-   if(valueInp1.sort== valueInp2.sort){
-
+   if(sortStrungCust(valueInp1) === sortStrungCust(valueInp2)){
       console.log("Its an anangram declarative : "+valueInp1+" : "+valueInp2);
 
    }else{
       console.log("Its not an anangram declarative : "+valueInp1+" : "+valueInp2);
 
    }
+   console.log("============");
 
 }
 
-isAnagramDeclarative("Masrsy","Arssmy");
+function sortStrungCust(strToSort) {
+   //Split to array sort and the join back to string
+   return strToSort.split('').sort().join('');
+};
+
+isAnagram("Mary","Army");
+isAnagram("Masrsy","Arswdy");
+isAnagram("alright","htgiral");
