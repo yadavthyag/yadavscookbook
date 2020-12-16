0c94c9cd0079b2560283405625372822c50791d0
yadavthyagaraj <74851892+yadavthyag@users.noreply.github.com>
2020-12-16 16:17:24 +0530

    anagram.js
    anagram.js

---
+++ recipes/anagram.js
@@ -0,0 +1,18 @@
+ 
+ function isAnagramDeclarative(valueInp1,valueInp2){
+   valueInp1 = valueInp1.toLowerCase();
+   valueInp2 = valueInp2.toLowerCase();
+
+
+   if(valueInp1.sort== valueInp2.sort){
+
+      console.log("Its an anangram declarative : "+valueInp1+" : "+valueInp2);
+
+   }else{
+      console.log("Its not an anangram declarative : "+valueInp1+" : "+valueInp2);
+
+   }
+
+}
+
+isAnagramDeclarative("Masrsy","Arssmy");
